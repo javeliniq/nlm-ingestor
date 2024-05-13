bbb3832f39af70dcbda816f50c479cd635b3e25a
Pratik Singh Chauhan <pratiksingh773@gmail.com>
2024-05-13 23:47:56 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 # latest version of java and a python environment where requirements are installed is required
 nohup java -jar jars/tika-server-standard-nlm-modified-2.4.1_v6.jar > /dev/null 2>&1 &
 # python -m nlm_ingestor.ingestion_daemon
-gunicorn -b 0.0.0.0:5001 -w 4 nlm_ingestor.ingestion_daemon:app
+gunicorn -b 0.0.0.0:5001 -w 4 nlm_ingestor.ingestion_daemon
 
